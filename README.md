1ddc59f15162319b600c14260523755a48984c11
A. Breust <alexis.breust@gmail.com>
2016-01-22 10:12:27 +0100

    Reorganized code + made a simpler example

--- README.md
+++ README.md
@@ -119,28 +119,6 @@ ls.addShape(lightShape);
 
 More instructions to come. You are of course welcome to post on the SFML forum thread for help: [http://en.sfml-dev.org/forums/index.php?topic=16895.0](http://en.sfml-dev.org/forums/index.php?topic=16895.0)
 
-License
------------
-
-LTBL2
-Copyright (C) 2014-2015 Eric Laukien
-
-This software is provided 'as-is', without any express or implied
-warranty.  In no event will the authors be held liable for any damages
-arising from the use of this software.
-
-Permission is granted to anyone to use this software for any purpose,
-including commercial applications, and to alter it and redistribute it
-freely, subject to the following restrictions:
-
-1. The origin of this software must not be misrepresented; you must not
-	claim that you wrote the original software. If you use this software
-	in a product, an acknowledgment in the product documentation would be
-	appreciated but is not required.
-2. Altered source versions must be plainly marked as such, and must not be
-	misrepresented as being the original software.
-3. This notice may not be removed or altered from any source distribution.
-
 ------------------------------------------------------------------------------
 
 LTBL2 uses the following external libraries:
---
+++ examples/basic.cpp
@@ -0,0 +1,110 @@
+#include <SFML/Graphics.hpp>
+
+#include <ltbl/lighting/LightSystem.h>
+
+int main(void)
+{
+    sf::RenderWindow window;
+    window.setVerticalSyncEnabled(true);
+    window.create(sf::VideoMode(1280, 720), "LTBL - Examples - Basic", sf::Style::Close);
+
+    sf::View view = window.getDefaultView();
+
+    //----- Initialize the light system
+
+    sf::RenderStates lightRenderStates;
+    lightRenderStates.blendMode = sf::BlendMultiply;
+
+    sf::Texture penumbraTexture;
+    penumbraTexture.loadFromFile("assets/penumbraTexture.png");
+    penumbraTexture.setSmooth(true);
+
+    sf::Shader unshadowShader;
+    unshadowShader.loadFromFile("assets/unshadowShader.vert", "assets/unshadowShader.frag");
+
+    sf::Shader lightOverShapeShader;
+    lightOverShapeShader.loadFromFile("assets/lightOverShapeShader.vert", "assets/lightOverShapeShader.frag");
+
+    ltbl::LightSystem ls;
+    ls.create(sf::FloatRect{{0.f, 0.f}, view.getSize()}, window.getSize(), penumbraTexture, unshadowShader, lightOverShapeShader);
+
+    //----- Add a light
+
+    sf::CircleShape pointLight(4.f);
+    pointLight.setFillColor(sf::Color::Blue);
+    pointLight.setOrigin(0.5f * pointLight.getLocalBounds().width, 0.5f * pointLight.getLocalBounds().height);
+
+    sf::Texture pointLightTexture;
+    pointLightTexture.loadFromFile("assets/pointLightTexture.png");
+    pointLightTexture.setSmooth(true);
+
+    auto light = std::make_shared<ltbl::LightPointEmission>();
+    light->_emissionSprite.setOrigin(pointLightTexture.getSize().x * 0.5f, pointLightTexture.getSize().y * 0.5f);
+    light->_emissionSprite.setTexture(pointLightTexture);
+    light->_emissionSprite.setScale(10.0f, 10.0f);
+    light->_emissionSprite.setColor({255u, 230u, 200u});
+    light->_emissionSprite.setPosition(100.0f, 100.0f);
+    ls.addLight(light);
+
+    //----- Add a square blocking light
+
+    sf::RectangleShape blocker;
+    blocker.setSize({200.f, 50.f});
+    blocker.setPosition(500.f, 300.f);
+    blocker.setFillColor(sf::Color::Red);
+
+    auto lightBlocker = std::make_shared<ltbl::LightShape>();
+    lightBlocker->_shape.setPointCount(4u);
+    lightBlocker->_shape.setPoint(0u, {0.f, 0.f});
+    lightBlocker->_shape.setPoint(1u, {0.f, blocker.getSize().y});
+    lightBlocker->_shape.setPoint(2u, blocker.getSize());
+    lightBlocker->_shape.setPoint(3u, {blocker.getSize().x, 0.f});
+    lightBlocker->_shape.setPosition(blocker.getPosition());
+    ls.addShape(lightBlocker);
+
+    //----- Program loop
+
+    bool quit = false;
+    while (!quit) {
+        // Inputs
+
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            switch (event.type) {
+            case sf::Event::Closed:
+                quit = true;
+                break;
+
+            case sf::Event::MouseMoved:
+                light->_emissionSprite.setPosition(event.mouseMove.x, event.mouseMove.y);
+                pointLight.setPosition(event.mouseMove.x, event.mouseMove.y);
+                break;
+
+            case sf::Event::KeyPressed:
+                if (event.key.code == sf::Keyboard::Escape)
+                    quit = true;
+                break;
+
+            default:
+                break;
+            }
+        }
+
+        // Normal rendering
+
+        window.clear(sf::Color::White);
+        window.draw(blocker);
+        window.draw(pointLight);
+
+        // Lighting rendering
+
+        ls.render(view, unshadowShader, lightOverShapeShader);
+
+        sf::Sprite sprite(ls.getLightingTexture());
+        window.draw(sprite, lightRenderStates);
+
+        window.display();
+    }
+
+    return EXIT_SUCCESS;
+}
